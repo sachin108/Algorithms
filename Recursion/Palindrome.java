808a31a2ad9913addf6786d9303a9f3bada6077d
sachin108 <88074105+sachin108@users.noreply.github.com>
2021-09-09 23:55:05 +0530

    Create Palindrome.java

---
+++ Recursion/Palindrome.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Palindrome{
+
+	static String reverse(String s,int index){
+
+		if(s.length()==0){
+
+			return "";
+		}
+		else{
+			String sub=s.substring(0,index);
+			return s.charAt(index)+reverse(sub,sub.length()-1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		String s=input.nextLine();
+		
+		String rev=reverse(s,s.length()-1);
+		
+		if(rev.equals(s)){
+
+			System.out.println("yes");
+		}
+		else{
+
+			System.out.println("no");
+		}
+	}
+}
+
+/**
+reverse 
+
+*/
