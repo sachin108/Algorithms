5f3e5acff99a2627ba90efa2a7ea7d57c6b9322b
sachin108 <sachin108rajput@hotmail.com>
2021-10-02 14:08:35 +0530

    updated

---
+++ Sorting_Algorithms/Insertion_sort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main() {
+    array<int,10> Array={122,3,14,61,41,78,51,90,73,21};
+    int length_of_array=Array.size();
+
+    // sorting 
+    for (int i = 0; i < length_of_array-1; i++){
+        for(int j=i+1; j>=0; j--){
+            if(Array[j-1]>Array[j]){
+                int temp=Array[j];
+                Array[j]=Array[j-1];
+                Array[j-1]=temp;
+            }
+        }
+    }
+    for(auto i : Array)
+    {
+        cout<<i<<"  ";
+    }
+    return 0;
+}
+/*
+
+*/
