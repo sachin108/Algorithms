f8a1e493401ea96a08f0a8ef6579618dc02d8e0b
sachin108 <sachin108rajput@hotmail.com>
2021-10-18 09:14:44 +0530

    updated

---
+++ Bitwise_Algorithms/unique_element.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int findUnique(vector<int> v);
+int main(){
+    vector<int> v={1,2,3,1,14,3,2,4,57,3,4,1,2,14};
+    int UniqueElement=findUnique(v);
+    cout<<"the unique elemment is "<<UniqueElement;
+    return 0;
+}
+int findUnique(vector<int> v){
+    int Unique=v[0];
+    vector<int>:: iterator itr;
+    itr=v.begin();
+    v.erase(itr);
+    for(int i: v){
+        Unique = Unique^i;
+    }
+    return Unique;
+}
+// there should be only one unique element in Array
--- Bitwise_Hacks/swapping.py
+++
@@ -1,7 +0,0 @@
-x=35
-y=12
-print("x = ",x," y = ",y)
-x=x^y 
-y=x^y 
-x=x^y 
-print("x = ",x," y = ",y)
